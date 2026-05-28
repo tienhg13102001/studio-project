962f82803933cf0ea1daec6dfae20b5abc77c3c5
Bryan_Tien <tienhg201@gmail.com>
2026-05-28 22:19:04 +0700

    fix

--- front-end/src/pages/ScriptPage.tsx
+++ front-end/src/pages/ScriptPage.tsx
@@ -2,7 +2,8 @@ const ScriptPage = () => {
   return (
     <iframe
       src="https://script.google.com/macros/s/AKfycbxpjBUuVXucUFAZvA--zEzUrW3gEFJxhSh24iGD42qHqRmp8eJn-FfyJCXmC6Ut1vs/exec"
-      className="fixed inset-0 z-9999 m-0 h-full w-full overflow-hidden border-none p-0"
+      className="block h-screen w-full border-none"
+      allowFullScreen
     ></iframe>
   );
 };
